7be9dab8145b66d7924f2d534d8d08f68b8a1ad0
krulik40000 <michalmalis@o2.pl>
2019-05-14 12:31:38 +0200

    similarityFinder test
    Współczynnik podobieństwa Jaccarda,
    test na takich samych sekwencjach

--- src/test/java/lab2_3/SimilarityFinderTest.java
+++ src/test/java/lab2_3/SimilarityFinderTest.java
@@ -33,4 +33,17 @@ public class SimilarityFinderTest {
         Assert.assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(equalTo(0d)));
     }
 
+    @Test
+    public void testJaccardSimilaritySameSeq() {
+        similarityFinder = new SimilarityFinder((key, seq) -> {
+            if (key == seq[0] || key == seq[1] || key == seq[2] || key == seq[3])
+                return SearchResult.builder()
+                                   .withFound(true)
+                                   .build();
+            return SearchResult.builder()
+                               .withFound(false)
+                               .build();
+        });
+        Assert.assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq1), is(equalTo(1d)));
+    }
 }
